de3e5f6de70e6705ce34453eb9d048962e0a5248
Laurent <laurent.tironi@gmail.com>
2021-06-04 16:18:46 +0000

    updated test file

--- hello.py
+++ hello.py
@@ -1,9 +1,5 @@
 def add(x, y):
     return x + y
 
-
-x = 1
-y = 2
-
-result = add(x, y)
-print(f"This is the sum: {x}, {y}, {result}")
+result = add(1, 2)
+print(f"This is the sum: 1, 2, {result}")
--- test_hello.py
+++ test_hello.py
@@ -2,5 +2,5 @@ from hello import add
 
 def test_add():
     assert add(1,2) == 3
-    assert add(1,2) == 4
+   
     
